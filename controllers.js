5849acc6f252306f9d023e55e61ad35779c7b0c1
Ken McGrady <ken.mcgrady@gmail.com>
2016-07-30 17:10:20 -0700

    Add $http

--- controllers.js
+++ controllers.js
@@ -2,30 +2,68 @@
   'use strict';
 
   const app = angular.module('todoApp');
-  app.controller('PeopleController', function() {
+  app.controller('PeopleCtrl', function($http) {
+    $http.defaults.headers.common['Content-Type'] = 'application/json'
     this.nameToAdd = '';
     this.people = [];
 
-    this.addPerson = function() {
-      this.people.push({ name: this.nameToAdd });
-      this.nameToAdd = '';
+    $http.get('https://galvanize-todos.herokuapp.com/km-persons')
+      .then((response) => {
+        this.people = response.data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    this.addPerson = () => {
+      $http.post('https://galvanize-todos.herokuapp.com/km-persons', {
+        name: this.nameToAdd
+      })
+      .then((response) => {
+        this.people.push(response.data);
+        this.nameToAdd = '';
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     };
   });
 
-  app.controller('TodoListController', function() {
+  app.controller('TodoListCtrl', function($scope, $http) {
+    this.person = $scope.person;
     this.todoToAdd = '';
     this.todos = [];
 
-    this.addTodo = function() {
-      this.todos.push({
-        completed: false,
-        text: this.todoToAdd
+    $http.get(`https://galvanize-todos.herokuapp.com/km-persons/${this.person.id}/km-todos`)
+      .then((response) => {
+        this.todos = response.data;
+      })
+      .catch((err) => {
+        console.error(err);
       });
-      this.todoToAdd = '';
+
+    this.addTodo = () => {
+      $http.post('https://galvanize-todos.herokuapp.com/km-todos', {
+        text: this.todoToAdd,
+        completed: false,
+        'km-personId': this.person.id
+      })
+      .then((response) => {
+        this.todos.push(response.data);
+        this.todoToAdd = '';
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     };
 
-    this.completeTodo = function(todo) {
-      todo.completed = true;
-    };
+    this.toggleCompleteTodo = (todo) => {
+      $http.patch(`https://galvanize-todos.herokuapp.com/km-todos/${todo.id}`, {
+        completed: todo.completed
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    }
   })
 }());
